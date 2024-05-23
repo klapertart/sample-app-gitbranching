331a83c0982f0b6b87a9e1163005e35998c23a48
otong.sunandar <otong.sunandar@tritronik.com>
2024-05-23 20:35:16 +0700

    initial commit

---
+++ src/main/java/com/klapertart/gitbranching/SampleAppGitbranchingApplication.java
@@ -0,0 +1,13 @@
+package com.klapertart.gitbranching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppGitbranchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppGitbranchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klapertart/gitbranching/SampleAppGitbranchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klapertart.gitbranching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleAppGitbranchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
